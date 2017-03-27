695b77e356a1b503f603ed264d01aedc3fa6bb0e
Quentin Calentier <Quentin Calentier>
2017-03-27 09:50:03 +0100

    continuing

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 var express = require('express')
 var app = express()
-app.set('view engine', 'hbs');
+var util = require('util')
+var Promise = require('bluebird');
+var rp = __dirname
+var fs = Promise.promisifyAll(require("fs"));
+var websites = new Object();
+var libxmljs = require("libxmljs");
+
+
+app.set('view engine', 'hbs')
+
+
+readManifests();
+
 
 app.get('/', function (req, res) {
-  res.send('WTF')
+    var hostname = req.headers.host;
+    res.send('WTF')
+
 })
- 
+
 app.listen(3000)
+
+
+function readManifests() {
+    var path = rp + "/websites/";
+    fs.readdirAsync(path).then(function (data) {
+        for (var n = 0; n < data.length; n++) {
+
+            var manifesto = path + data[n] + '/manifest.xml';
+            fs.readFileAsync(manifesto).then(function (data) {
+                var doc = libxmljs.parseXml(data);
+                var domainid = doc.get('//domain');
+                websites[domainid.text()] = doc;
+            })
+
+        }
+    })
+
+
+}
+
+// TEMPORARY LOG AREA
+console.log(rp);
