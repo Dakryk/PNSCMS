a1cf0597a3f05a317ea216e8db52467a44afbcef
Dakryk <q.calentier@gmail.com>
2017-03-31 08:10:53 +0100

    continuing

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var Promise = require('bluebird');
 var rp = __dirname
 var fs = Promise.promisifyAll(require("fs"));
 var websites = new Object();
-// var libxmljs = require("libxmljs");
+var libxmljs = require("libxmljs");
 var QAPI = new Object();
 var SBCache = require("smartbridge_cache");
+var SBRouting = require("smartbridge_routing");
+var parseString = require('xml2js').parseString;
+
 QAPI.utils = require("smartbridge_utilities");
 app.set('view engine', 'hbs')
 
@@ -25,21 +28,32 @@ app.listen(3000)
 
 
 function readManifests() {
-/*    var path = rp + "/websites/";
+    var path = rp + "/websites/";
     fs.readdirAsync(path).then(function (data) {
         for (var n = 0; n < data.length; n++) {
 
             var manifesto = path + data[n] + '/manifest.xml';
             fs.readFileAsync(manifesto).then(function (data) {
-                var doc = libxmljs.parseXml(data);
-                var domainid = doc.get('//domain');
-                websites[domainid.text()] = doc;
+                parseString(data, function (err, result) {
+        var r = result.configuration.routing[0].routes;
+        var rcnt = r.length;
+                    
+                    for (var u = 0; u < rcnt; u++){
+        var p = result.configuration.routing[0].routes[u].route;
+                        console.log(p);
+                    }
+            
+//    console.dir(result.configuration.routing[0].routes[0].route);
+});
+            
+        
+        //        SBRouting.buildroutes(doc);
             })
 
         }
     })
 
-*/
+
 }
 
 // TEMPORARY LOG AREA
